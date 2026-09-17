a597b35104ffb29b3b3dcd9222d41fc4b2cebf9d
Abha Bhole <ab2792@cornell.edu>
2026-09-17 15:53:21 -0400

    added impact guard to poincare script

--- assignment_2_poincare.py
+++ assignment_2_poincare.py
@@ -19,8 +19,9 @@ def poincare_section_crossed(previous_state, next_state):
     return (previous_theta < section_theta and next_theta >= section_theta)
 
 
-def simulate_poincare(initial_state):
+def simulate_poincare(initial_state, alpha):
     local_params = params.copy()
+    local_params["angle_of_attack"] = alpha
 
     n_timesteps = round(sim_time / timestep)
 
@@ -31,20 +32,30 @@ def simulate_poincare(initial_state):
     for index in range(n_timesteps):
         next_state = state + timestep * model.dynamics(index * timestep, state, local_params,)
 
+        # Check for Poincaré-section crossing
         if poincare_section_crossed(state, next_state):
             print(f"Crossing at t = {index * timestep:.3f}, "f"theta_dot = {next_state[1]:.4f}")
             poincare_velocities.append(next_state[1])
+
+        # Check for impact
+        if model.event_guard(state, next_state, local_params):
+            print(f"Impact at t = {index * timestep:.3f}, "f"theta = {next_state[0]:.4f}")
+
+            next_state = model.event_dynamics(next_state, local_params,)
 
         state = next_state
 
     return np.array(poincare_velocities)
 
 
+
 if __name__ == "__main__":
 
-    initial_state = [-0.1, 1.0]
+    alpha = params["angle_of_attack"]
 
-    poincare_velocities = simulate_poincare(initial_state)
+    initial_state = [0.0, 1.0]
+
+    poincare_velocities = simulate_poincare(initial_state, alpha,)
 
     print("Poincaré section:")
     print(poincare_velocities)
